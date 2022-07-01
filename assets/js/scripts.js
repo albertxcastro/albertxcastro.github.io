87708004cbae7c9f00e78a72223cca053dcf010b
Alberto Castro <bcastroe@outlook.com>
2022-06-30 21:27:51 -0500

    Add dummy file for MSc section

---
+++ assets/js/scripts.js
@@ -0,0 +1,12 @@
+function showOrHide() {
+    var x = document.getElementById("expId");
+    var btn = document.getElementById("expBtn");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+        btn.innerHTML = "Show less";
+    } else {
+        x.style.display = "none";
+        btn.innerHTML = "Show more";
+    }
+  }
+  
